928e4fa98e19130d28ca7b9e9e1d452ece827738
SATENDRA1111 <117499684+SATENDRA1111@users.noreply.github.com>
2023-05-28 17:06:49 +0530

    server.js

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ import { connectdb } from "./data/database.js";
 connectdb();
 // process.env.port = 4000
 app.listen(process.env.port,()=> {
-     console.log('server is working on port '+process.env.port +process.env.NODE_ENV );
-})
+  //   console.log('server is working on port '+process.env.port +process.env.NODE_ENV );
+      console.log(
+    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
+  );
+}
